015e0e2826f9b6bceee07d7dd66462cc6b2e51a6
AntiMatr126 <106202625+AntiMatr126@users.noreply.github.com>
2022-06-18 11:07:31 -0600

    Update script.js

--- V1/js/script.js
+++ V1/js/script.js
@@ -7,3 +7,8 @@ function decode() {
     let string = prompt('Enter Cypher:','-493 0 119 119 170 -1173 -238 170 221 119 -17');
     alert(Encode.NumToLtr(string))
 }
+
+window.onload = () => {
+    document.getElementById('encode').style.fontSize = `${window.screen.width/20}px`;
+    document.getElementById('decode').style.fontSize = `${window.screen.width/20}px`;
+}
