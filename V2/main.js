5a391e1fabf557c0011a8dcf530e24e86c8de3a8
AntiMatr126 <106202625+AntiMatr126@users.noreply.github.com>
2023-06-05 13:02:46 -0600

    Add files via upload
    Copies code to slipboard

--- V2/main.js
+++ V2/main.js
@@ -2,16 +2,25 @@ function encode () {
 	var inp = document.getElementById('inp').value;
 	var type = document.getElementById('type').value;
 
-	document.getElementById('out').innerText = CN[`_${type}`].encode(inp,undefined,true);
+	inp = CN[`_${type}`].encode(inp,undefined,true)
+	document.getElementById('out').innerText = inp;
+
+	copyText(inp);
 }
 function decode () {
 	var inp = document.getElementById('inp').value;
 	var type = document.getElementById('type').value;
 
 	try {
-		document.getElementById('out').innerText = CN[`_${type}`].decode(inp);
+		inp = CN[`_${type}`].decode(inp)
+		document.getElementById('out').innerText = inp;
 	} catch (err) {
 		inp = CN._1.fix(inp);
+		inp = CN[`_${type}`].decode(inp)
 		document.getElementById('out').innerText = CN[`_${type}`].decode(inp);
 	}
+}
+
+var copyText = function (text) {
+	navigator.clipboard.writeText(text);
 }
