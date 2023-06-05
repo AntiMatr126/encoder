6d63ce006e94aa3a88ce32498bd2d97666710a39
AntiMatr126 <106202625+AntiMatr126@users.noreply.github.com>
2023-06-05 10:53:35 -0600

    Update main.js

--- V2/main.js
+++ V2/main.js
@@ -2,7 +2,7 @@ function encode () {
 	var inp = document.getElementById('inp').value;
 	var type = document.getElementById('type').value;
 
-	document.getElementById('out').innerText = CN[`_${type}`].encode(inp);
+	document.getElementById('out').innerText = CN[`_${type}`].encode(inp,undefined,true);
 }
 function decode () {
 	var inp = document.getElementById('inp').value;
