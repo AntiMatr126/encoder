d19b68d60917f5f6e0af54ca1087360bbe3052ff
AntiMatr126 <106202625+AntiMatr126@users.noreply.github.com>
2023-08-11 13:40:15 -0600

    Added CN2, also, CN1 doesn't work now

--- V2/main.js
+++ V2/main.js
@@ -1,8 +1,13 @@
+const funcVars = {
+	_1: [undefined,true]
+}
+
 function encode () {
 	var inp = document.getElementById('inp').value;
 	var type = document.getElementById('type').value;
 
-	inp = CN[`_${type}`].encode(inp,undefined,true)
+	var vars = (typeof funcVars[`_${type}`] == 'undefined') ? undefined : [...funcVars[`_${type}`]]
+	inp = CN[`_${type}`].encode(inp,vars)
 	document.getElementById('out').innerText = inp;
 
 	copyText(inp);
@@ -11,16 +16,25 @@ function decode () {
 	var inp = document.getElementById('inp').value;
 	var type = document.getElementById('type').value;
 
-	try {
-		inp = CN[`_${type}`].decode(inp)
-		document.getElementById('out').innerText = inp;
-	} catch (err) {
-		inp = CN._1.fix(inp);
-		inp = CN[`_${type}`].decode(inp)
-		document.getElementById('out').innerText = CN[`_${type}`].decode(inp);
+	var cn = CN[`_${type}`];
+
+	switch(type) {
+		case '1':
+			try {
+				inp = cn.decode(inp)
+			} catch (err) {
+				inp = cn.fix(inp);
+				inp = cn.decode(inp)
+				inp = cn.decode(inp);
+			}
+			break;
+		default:
+			inp = cn.decode(inp);
 	}
+
+	
+	document.getElementById('out').innerText = inp;
+
 }
 
-var copyText = function (text) {
-	navigator.clipboard.writeText(text);
-}
+var copyText = text => navigator.clipboard.writeText(text);
